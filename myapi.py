4b50dee889a21ff2d71befc92e720b27fcc2d205
Paul-byte59 <paulokoho21@gmail.com>
2022-09-26 19:01:12 -0400

    6th commit

--- myapi.py
+++ myapi.py
@@ -1,20 +1,21 @@
+from pathlib import Path
 from fastapi import FastAPI
 
 app = FastAPI()
 
 students = {
     1:{
-        "name": "john"'
-        "age": 18
-        "class": "Year 15"
+        "name": "john",
+        "age": 18,
+        "class": "Year 15",
         "sex": "male"
     }
 }
 
-@app.get("/")
-def index():
-    return {"name": "First Name"}
+#@app.get("/")
+#def index():
+ #   return {"name": "First Name"}
 
 @app.get("/get-student/{student_id}")
-def get_student(student_id: int):
+def get_student(student_id: int = Path(None, description="the ID of the student you want to view", gt=0 lt=3)):
     return students[student_id]
