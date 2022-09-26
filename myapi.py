620ea1e7b815a1eecbb396d51eb9db00eaa841d3
Paul-byte59 <paulokoho21@gmail.com>
2022-09-26 18:48:17 -0400

    5th commit

--- myapi.py
+++ myapi.py
@@ -2,6 +2,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+students = {
+    1:{
+        "name": "john"'
+        "age": 18
+        "class": "Year 15"
+        "sex": "male"
+    }
+}
+
 @app.get("/")
 def index():
     return {"name": "First Name"}
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int):
+    return students[student_id]
